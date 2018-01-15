f4629ae909f60aba735a2b51cfbf5beb43163ee5
KonradLekawski <Snajperv52@gmail.com>
2018-01-15 16:01:39 +0100

    change implementation of FileReader

--- src/main/java/com/codecool/scc/daos/FileReader.java
+++ src/main/java/com/codecool/scc/daos/FileReader.java
@@ -1,40 +1,19 @@
 package com.codecool.scc.daos;
 
+import com.opencsv.CSVReader;
 import org.springframework.stereotype.Component;
 
+
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+
 
 @Component
 public class FileReader {
 
     public List<String[]> readData(String file) throws IOException {
-        List<String[]> table = new ArrayList<>();
-
-        String fileContent = readLineByLine(file);
-
-        String[] rows= fileContent.split("[\\r\\n]+");
-
-        for (String row : rows) {
-            table.add(row.split("\\s*,\\s*"));
-        }
-
-        return table;
-    }
-
-    private String readLineByLine(String file) throws IOException {
-        StringBuilder fileContent = new StringBuilder();
-
-        Stream<String> stream = Files.lines( Paths.get(file), StandardCharsets.UTF_8);
-
-        stream.forEach(s -> fileContent.append(s).append("\n"));
-
-        return fileContent.toString();
+        CSVReader reader = new CSVReader(new java.io.FileReader(file));
+        return reader.readAll();
     }
 
 }
